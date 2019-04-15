d489c979461dae36abb4902f24e8fcfb60ed7e6c
JaloDeng <651379728@qq.com>
2019-04-15 17:00:53 +0800

    优化活动查询接口

--- src/main/java/com/newgen/mapper/ActivityMapper.java
+++ src/main/java/com/newgen/mapper/ActivityMapper.java
@@ -1,11 +1,7 @@
 package com.newgen.mapper;
 
-import java.util.List;
-import java.util.Map;
-
 import com.newgen.bean.Activity;
 
 public interface ActivityMapper extends BaseMapper<Activity> {
 	
-	List<Map<?, ?>> findListForSearch(Map<String, Object> var1);
 }
--- src/main/java/com/newgen/service/ActivityService.java
+++ src/main/java/com/newgen/service/ActivityService.java
@@ -1,9 +1,5 @@
 package com.newgen.service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,15 +16,6 @@ public class ActivityService extends BaseService<Activity> {
 	@Override
 	public BaseMapper<Activity> getMapper() {
 		return this.activityMapper;
-	}
-	
-	public List<Map<?, ?>> findListForSearch(Integer page, Integer row) {
-		Map<String, Object> params = new HashMap<String, Object>();
-		if (page != null && row != null) {
-			params.put("index", (page - 1)*row);
-			params.put("row", row);
-		}
-		return activityMapper.findListForSearch(params);
 	}
 	
 }
