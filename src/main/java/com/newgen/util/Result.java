bc7fc2c263ae46484a9c180aa62457e0c767b80e
JaloDeng <651379728@qq.com>
2019-04-18 17:57:50 +0800

    添加返回结果类

---
+++ src/main/java/com/newgen/util/Result.java
@@ -0,0 +1,46 @@
+package com.newgen.util;
+
+public class Result {
+
+	private Integer ret;
+	
+	private String msg;
+	
+	private Object data;
+
+	public Result(Integer ret, String msg, Object data) {
+		this.ret = ret;
+		this.msg = msg;
+		this.data = data;
+	}
+
+	public Integer getRet() {
+		return ret;
+	}
+
+	public void setRet(Integer ret) {
+		this.ret = ret;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		return "Result [ret=" + ret + ", msg=" + msg + ", data=" + data + "]";
+	}
+	
+}
