54eedbeff4c3e47226f0229ee4eadf52a5be95c9
JaloDeng <651379728@qq.com>
2019-04-24 12:58:55 +0800

    添加后台管理系统backmanager

---
+++ backmanager/src/main/java/com/newgen/backmanager/BackmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.newgen.backmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackmanagerApplication.class, args);
+	}
+
+}
---
+++ backmanager/src/test/java/com/newgen/backmanager/BackmanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.newgen.backmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackmanagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
