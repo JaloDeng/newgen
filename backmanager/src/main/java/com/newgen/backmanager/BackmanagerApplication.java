bab2f86b8fe82bffd76dd2880bbc830c245e8aa8
JaloDeng <651379728@qq.com>
2019-04-26 10:51:11 +0800

    backmanager: 添加底层commons项目的mybatis扫描配置

--- backmanager/src/main/java/com/newgen/backmanager/BackmanagerApplication.java
+++ backmanager/src/main/java/com/newgen/backmanager/BackmanagerApplication.java
@@ -3,6 +3,7 @@ package com.newgen.backmanager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -12,8 +13,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @time 2019-04-25 10:38:59
  */
 
+@ComponentScan(basePackages = "com.newgen")
 @EnableSwagger2
-@MapperScan("com.newgen.backmanager.mapper")
+@MapperScan({"com.newgen.backmanager.mapper", "com.newgen.commons.mapper"})
 @SpringBootApplication
 public class BackmanagerApplication {
 
